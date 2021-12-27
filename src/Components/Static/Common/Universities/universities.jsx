87c8a2c183acc8cd79d7345b2509975568179d27
Konstaphy <konstafyf@gmail.com>
2021-12-27 23:34:09 +0500

    huynya

--- src/Components/Static/Common/Universities/universities.jsx
+++ src/Components/Static/Common/Universities/universities.jsx
@@ -6,7 +6,17 @@ export class Universities extends Component {
   render() {
     return (
       <div className={classes.uni}>
-        <TitleBar setClosed={this.props.setClosed} title={"Места для обучения"} />
+        <TitleBar
+          setClosed={this.props.setClosed}
+          title={"Места для обучения"}
+        />
+        <div className={classes.content}>
+          <div className={classes.title}>Места обучения</div>
+          <ul className={classes.list}>
+            <li>Через университет:</li>
+            <li>Через самооброазование:</li>
+          </ul>
+        </div>
       </div>
     );
   }
