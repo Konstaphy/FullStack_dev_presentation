13835787c5cdd2dddf3cb32c26c593ea104be5e7
Konstantine <Konstaphy@gmail.com>
2022-01-02 15:03:58 +0500

    Develop (#18)

--- src/Components/Static/Common/AverageSalary/averageSalary.jsx
+++ src/Components/Static/Common/AverageSalary/averageSalary.jsx
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
 import classes from "./averageSalary.module.css";
 import { TitleBar } from "../../../Technologies/TitleBar/titleBar";
+import salary from "../../../../Assets/salary.png";
 
 export class AverageSalary extends Component {
   render() {
     return (
       <div className={classes.salo}>
         <TitleBar setClosed={this.props.setClosed} title={"Средняя зп"} />
+        <div className={classes.salary__content}>
+          <div className={classes.text}>
+            <h1>Средння цена зависит от нескольких факторов:</h1>
+            <ul className={classes.dependencies}>
+              <li>- Регион</li>
+              <li>- Опыт</li>
+              <li>- Требуемый стек технологий</li>
+            </ul>
+          </div>
+          <div className={classes.salary__data}>
+            <img src={salary} alt="salary" className={classes.salary__image} />
+          </div>
+        </div>
       </div>
     );
   }
--- src/Components/Static/Common/Universities/universities.jsx
+++ src/Components/Static/Common/Universities/universities.jsx
@@ -6,7 +6,18 @@ export class Universities extends Component {
   render() {
     return (
       <div className={classes.uni}>
-        <TitleBar setClosed={this.props.setClosed} title={"Места для обучения"} />
+        <TitleBar
+          setClosed={this.props.setClosed}
+          title={"Места для обучения"}
+        />
+        <div className={classes.content}>
+          <div className={classes.title}>Места обучения</div>
+          Обучиться на Full stack разработчика можно в университетах, а так-же
+          на различных курсах. <br /> <strong> Онлайн курсы: </strong>
+          1) Курсы от GeekBrains <br /> 2) Курсы от Skillbox
+          <br /> 3) Курсы от Специалист.ru Учебного центра при МГТУ им. Баумана{" "}
+          <br /> Университеты: <br /> 1) МГУ <br /> 2) СПБГУ <br /> 3) УРФУ
+        </div>
       </div>
     );
   }
