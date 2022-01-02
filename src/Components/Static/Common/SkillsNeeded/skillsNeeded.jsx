c89dbbf64e786c9b72d192a4634f91b0c72dedc1
Konstaphy <konstafyf@gmail.com>
2022-01-02 15:03:35 +0500

    fsdfs

--- src/Components/Static/Common/SkillsNeeded/skillsNeeded.jsx
+++ src/Components/Static/Common/SkillsNeeded/skillsNeeded.jsx
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
-import classes from "./skillsNeeded.css";
+import classes from "./skillsNeeded.module.css";
 import { TitleBar } from "../../../Technologies/TitleBar/titleBar";
 
 export class SkillsNeeded extends Component {
   render() {
     return (
       <div className={classes.skills}>
-        <TitleBar setClosed={this.props.setClosed} title={"Необходимые способности"} />
+        <TitleBar
+          setClosed={this.props.setClosed}
+          title={"Необходимые способности"}
+        />
+        <div className={classes.content}>
+          <strong>Для Full Stack разработчика требуются навыки:</strong>
+          <ul className={classes.liststs}>
+            <li>Коммуникабельность</li>
+            <li>Уравновешанность</li>
+            <li>Умение широко и креативно мыслить</li>
+            <li>Понимание порграммирования и основных принципов</li>
+            <li>Готовность решать сложные задачи в короткий срок</li>
+            <li>Навыки дизайна</li>
+            <li>Умение работать в команде</li>
+            <li>Умение работать на нескольких проектах одновременно</li>
+          </ul>
+        </div>
       </div>
     );
   }
--- src/Components/Static/Common/Universities/universities.jsx
+++ src/Components/Static/Common/Universities/universities.jsx
@@ -12,10 +12,11 @@ export class Universities extends Component {
         />
         <div className={classes.content}>
           <div className={classes.title}>Места обучения</div>
-          <ul className={classes.list}>
-            <li>Через университет:</li>
-            <li>Через самооброазование:</li>
-          </ul>
+          Обучиться на Full stack разработчика можно в университетах, а так-же
+          на различных курсах. <br /> <strong> Онлайн курсы: </strong>
+          1) Курсы от GeekBrains <br /> 2) Курсы от Skillbox
+          <br /> 3) Курсы от Специалист.ru Учебного центра при МГТУ им. Баумана{" "}
+          <br /> Университеты: <br /> 1) МГУ <br /> 2) СПБГУ <br /> 3) УРФУ
         </div>
       </div>
     );
